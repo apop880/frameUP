c3240be23d672b7a9b9d053735c992741d0a0a14
apop880 <ajpopoutsis@gmail.com>
2021-03-05 05:41:25 +0000

    Create main grid layout

--- src/App.svelte
+++ src/App.svelte
@@ -1,20 +1,14 @@
 <script>
 	import { stateStore, action } from './store.js'
 	import BackgroundSlideshow from './components/slideshow.svelte'
+	import Topbar from './components/topbar.svelte';
 </script>
 
 <main>
-	{#if $stateStore !== null}
-	<h1>Hello world {$stateStore['light.theater'].state}!</h1>
-	{:else}
-	Loading...
-	{/if}
-	<!--
-	<button on:click="{e => action(['light', 'toggle', 'light.theater'])}">Call Service</button>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-	-->
-	<BackgroundSlideshow />
+	<Topbar />
+	
 </main>
+<BackgroundSlideshow />
 
 <style>
 	:global(body) {
@@ -22,22 +16,14 @@
 	}
 
 	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
+		display: grid;
+		grid-template-columns: 1fr;
+		grid-template-rows: 80px 1fr 100px;
+		gap: 0px 0px;
+		grid-template-areas:
+			"."
+			"."
+			".";
+		height: 100vh;
 	}
 </style>
