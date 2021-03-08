ef87435b5378150b4383c027e3b0cdb6509a0541
apop880 <ajpopoutsis@gmail.com>
2021-03-08 14:27:01 +0000

    Randomize photo slideshow

--- photo_array.py
+++ photo_array.py
@@ -1,4 +1,5 @@
 import os, os.path
+import random
 import json
 
 list_dir = []
@@ -6,6 +7,8 @@ slides = []
 list_dir = os.listdir("/root/frameup/public/bg_images")
 for file in list_dir:
     slides.append("bg_images/"+file)
+
+random.shuffle(slides)
 
 with open("/root/frameup/public/slideshow.json", "w") as filehandle:
     json.dump(slides, filehandle)
