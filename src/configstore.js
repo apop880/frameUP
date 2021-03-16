44ea901efbe8dda04e04840a8f2d29ae93aeaf63
apop880 <ajpopoutsis@gmail.com>
2021-03-16 18:55:25 +0000

    Refactor configstore

--- src/configstore.js
+++ src/configstore.js
@@ -1,22 +1,22 @@
 import { writable } from 'svelte/store';
 
 const configStore = writable('');
+const configName = localStorage.getItem('configName');
 
-async () => {
-	//has this device gotten a named config yet?
-	configName = localStorage.getItem('configName');
-	if (configName) {
-		//load in config
-		const res = await fetch('/get_config', {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json'
-			},
-			body: {"name": configName}
-		})
-		config = await res.json();
-		store.updateConfig(configName, config)
-	}
+if(configName) {
+	loadConfig(configName);
+}
+
+const loadConfig = async (configName) => {
+	const res = await fetch('/get_config', {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json'
+		},
+		body: {"name": configName}
+	})
+	config = await res.json();
+	store.updateConfig(configName, config);
 }
 
 const updateConfig = (configName, configData) => {
