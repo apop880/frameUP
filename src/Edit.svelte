7124d59d0c8d7f1a40e077dbc4993aba4d203b2f
apop880 <ajpopoutsis@gmail.com>
2021-03-19 05:01:41 +0000

    Pull in entities to editor

--- src/Edit.svelte
+++ src/Edit.svelte
@@ -1,7 +1,28 @@
 <script>
     import { configStore, saveConfig } from './configstore.js';
+    import { stateStore } from './apistore.js';
     import SelectModal from './components/selectmodal.svelte';
+    import { onMount } from 'svelte';
     import { weather } from "./config.js";
+    import { notifications } from './config.js';
+
+    let entities = null;
+
+    let unsubscribe;
+
+    onMount(() => {
+        unsubscribe = stateStore.subscribe(value => {
+            if(entities === null) {
+                entities = value;
+                entities = entities;
+            }
+        })
+    })
+
+    $: if(entities !== null) {
+        unsubscribe();
+        console.log(entities['light.bar_bulb']);
+    }
 
     let configList = [];
 
@@ -18,14 +39,24 @@
 <SelectModal on:message={handleMessage} />
 {:else}
 <main>
-    <div class="controls"><button on:click={saveConfig}>Save Changes</button> <button on:click={() => loadNew = true}>Load New</button> <button on:click={() => $configStore.mode = "view"}>Close Editor</button></div>
+    <div class="controls"><button on:click={saveConfig}>Save Changes</button> <button on:click={() => loadNew = true}>Load New</button> <button on:click={() => location.reload()}>Close Editor</button></div>
     <div class="edit">
-    <form>
-    <h3>Weather</h3>
-        <!-- svelte-ignore a11y-label-has-associated-control -->
-        <label>Temperature Sensor</label>
-        <input type="text" bind:value={$configStore.data.weather_entity} />
-    </form>
+    {#if entities !== null}
+        <form>
+        <h3>Weather Entity</h3>
+            <select bind:value={$configStore.data.weather_entity}>
+                {#each Object.keys(entities) as key}
+                    {#if key.startsWith('weather')}
+                        <option value={key}>{key}</option>
+                    {/if}
+                {/each}
+            </select>
+        <h3>Sensors to Monitor</h3>
+        <!-- {#each Object.keys($configStore.data.sensors) as key }
+            <select></select>
+        {/each} -->
+        </form>
+    {/if}
     </div>
     <div class="nav" />
 </main>
@@ -36,6 +67,7 @@
         justify-content: center;
         display: grid;
         grid-template-rows: 80px 1fr 100px;
+        grid-template-columns: 1fr;
         gap: 0px 0px;
         height: 100vh;
     }
@@ -47,7 +79,12 @@
         border: none;
     }
 
+    .controls {
+        justify-self: center;
+    }
+
     .edit {
         background-color: rgb(184, 184, 184);
+        padding: 10px;
     }
 </style>
